bd6c04d33cd8ee0cb929993292bd6f8c078fb147
Patrick Stadler <patrick.stadler@gmail.com>
2022-08-12 15:27:55 +0200

    allow custom Ajv instance (closes #115)

--- test/validator.test.js
+++ test/validator.test.js
@@ -1,8 +1,13 @@
 const { test } = require("tap");
 const Ajv = require("ajv");
+const Ajv2019 = require("ajv/dist/2019");
 
 const { Validator } = require("../src");
 
 test("Validator instance", async t => {
 	t.type(new Validator().ajv, Ajv, " property `ajv` should be an Ajv instance");
+});
+
+test("Validator instance w/ custom AJV instance", async t => {
+	t.type(new Validator(new Ajv2019()).ajv, Ajv2019, " property `ajv` should be an Ajv2019 instance");
 });
